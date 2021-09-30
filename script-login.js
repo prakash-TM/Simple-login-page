163185e931f3f6212c8ed1ee637833fff0f318d7
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2021-09-30 15:19:41 +0530

    added js codes

--- script-login.js
+++ script-login.js
@@ -1,29 +1,3 @@
-fetch("https://api.publicapis.org/entries")
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("NETWORK RESPONSE ERROR");
-        }
-    })
-    .then(data => {
-
-        a = (JSON.stringify(data));
-        // for (let i = 0; i < 100; i++) {
-        //     document.writeln(a[i]);
-        // }
-
-        document.write(a);
-
-        displayCocktail(data)
-    })
-    .catch((error) => console.error("FETCH ERROR:", error));
-
-
-
-
-// loging cheak
-
 var emailError;
 var emailCheck;
 
